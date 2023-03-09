7dd679c995cba7bec32848581b8cb248197acc73
Gwenitora <gwenitora.gardu74@gmail.com>
2023-03-09 21:14:51 +0100

    Debug : double pointer on init window

--- Demineur/MySDL.h
+++ Demineur/MySDL.h
@@ -3,7 +3,7 @@
 #include <SDL.h>
 #include "DynamicArray.h"
 
-void InitDemineurWindow(SDL_Renderer* renderer, SDL_Window* window, int sizeX, int sizeY);
+void InitDemineurWindow(SDL_Renderer** renderer, SDL_Window** window, int sizeX, int sizeY);
 void DestroyDemineurWindow(SDL_Renderer* renderer, SDL_Window* window);
 void Drawn(DynamicArray* cell, SDL_Renderer* rend);
 
